b8c10fddd236632fb36835ca6bfdf44bedea177d
JHyunB <whdgus4892@gmail.com>
2020-08-11 16:12:39 +0900

    Add MemberServiceTest

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void join() throws Exception{
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertEquals(member,memberRepository.findOne(savedId));
+    }
+
+    @Test
+    void duplicate() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+}
